a2a496fe07a3f7968f96059a7e020062eff54ed9
TomLefley <thomas.lefley@gmail.com>
2014-07-15 11:02:20 +0100

    Web.xml changed in favour of java classes for servlet registering

---
+++ ETick/src/main/java/uk/ac/cam/cl/ticking/ui/api/ETickApiFacade.java
@@ -0,0 +1,5 @@
+package uk.ac.cam.cl.ticking.ui.api;
+
+public class ETickApiFacade {
+
+}
---
+++ ETick/src/main/java/uk/ac/cam/cl/ticking/ui/api/ETickApplicationRegister.java
@@ -0,0 +1,44 @@
+package uk.ac.cam.cl.ticking.ui.api;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import uk.ac.cam.cl.ticking.ui.auth.RavenViewTest;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+/**
+ * This class registers the resteasy handlers. The name is important since
+ * it is used as a String in HttpServletDispatcherV3
+ * 
+ * @author tl364
+ * 
+ */
+public class ETickApplicationRegister extends Application {
+
+	private Set<Object> singletons;
+	
+	/**
+	 * Default constructor
+	 */
+	public ETickApplicationRegister() {
+		singletons = new HashSet<Object>();
+	}
+	
+	@Override
+	public final Set<Object> getSingletons() {
+		Injector injector = Guice
+				.createInjector(new ETickGuiceConfigurationModule());
+		this.singletons.add(new RavenViewTest());
+		return this.singletons;
+	}
+	
+	@Override
+	public final Set<Class<?>> getClasses() {
+		Set<Class<?>> result = new HashSet<Class<?>>();
+		return result;
+	}
+}
