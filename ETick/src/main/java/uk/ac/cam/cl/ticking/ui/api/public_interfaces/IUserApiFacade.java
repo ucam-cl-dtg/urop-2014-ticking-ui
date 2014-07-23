14ec01487e8427bce82b761ca5d46abce5b70ebe
TomLefley <thomas.lefley@gmail.com>
2014-07-23 09:43:29 +0100

    Morning merge

--- ETick/src/main/java/uk/ac/cam/cl/ticking/ui/api/public_interfaces/IUserApiFacade.java
+++ ETick/src/main/java/uk/ac/cam/cl/ticking/ui/api/public_interfaces/IUserApiFacade.java
@@ -1,17 +1,12 @@
 package uk.ac.cam.cl.ticking.ui.api.public_interfaces;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
-
-import uk.ac.cam.cl.ticking.ui.actors.Grouping;
-import uk.ac.cam.cl.ticking.ui.actors.Role;
 
 @Path("/user")
 @Produces("application/json")
