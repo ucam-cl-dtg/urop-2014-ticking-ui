fe4c37986af25fd19aa19c18a8abc40bdff9ecbf
TomLefley <thomas.lefley@gmail.com>
2014-08-04 11:58:18 +0100

    pre merge

---
+++ ETick/src/test/java/uk/ac/cam/cl/ticking/ui/actors/UserTest.java
@@ -0,0 +1,14 @@
+package uk.ac.cam.cl.ticking.ui.actors;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
