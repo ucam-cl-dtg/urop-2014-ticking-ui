1e0076454c827355ef94f82c837ccc857804d153
TomLefley <thomas.lefley@gmail.com>
2014-08-04 12:12:50 +0100

    100% more unit tests

--- ETick/src/test/java/uk/ac/cam/cl/ticking/ui/actors/UserTest.java
+++ ETick/src/test/java/uk/ac/cam/cl/ticking/ui/actors/UserTest.java
@@ -8,7 +8,28 @@ public class UserTest {
 
 	@Test
 	public void testCompareTo() {
-		fail("Not yet implemented");
+		User first;
+		User second;
+		
+		first = new User("aa123");
+		first.setSurname("aardvark");
+		second = new User("zz123");
+		second.setSurname("zebra");
+		assertTrue("Testing with both having both fields", (first.compareTo(second)<0));
+		
+		first = new User("aa123");
+		first.setSurname("aardvark");
+		second = new User("zz123");
+		assertTrue("Testing with second lacking surname", (first.compareTo(second)<0));
+		
+		first = new User("aa123");
+		second = new User("zz123");
+		second.setSurname("zebra");
+		assertTrue("testing with first lacking surname", (first.compareTo(second)<0));
+		
+		first = new User("aa123");
+		second = new User("zz123");
+		assertTrue("tesing with no surnames", (first.compareTo(second)<0));
 	}
 
 }
