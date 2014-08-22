c8daed91bc103d5e57442e78f06dc43693d5249e
TomLefley <thomas.lefley@gmail.com>
2014-08-22 16:51:50 +0100

    trying to fix deploy

--- ETick/src/main/java/uk/ac/cam/cl/ticking/ui/api/util/HttpServletDispatcherV3.java
+++ ETick/src/main/java/uk/ac/cam/cl/ticking/ui/api/util/HttpServletDispatcherV3.java
@@ -6,9 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
 
 @WebServlet(urlPatterns = { "/api/*" }, initParams = {
-		@WebInitParam(name = "javax.ws.rs.Application", value = "uk.ac.cam.cl.ticking.ui.api.ApplicationRegister"),
+		@WebInitParam(name = "javax.ws.rs.Application", value = "uk.ac.cam.cl.ticking.ui.api.util.ApplicationRegister"),
 		@WebInitParam(name = "resteasy.servlet.mapping.prefix", value = "/api") })
 public class HttpServletDispatcherV3 extends HttpServletDispatcher {
 
-	private static final long serialVersionUID = -4372001075760879707L;
+	private static final long serialVersionUID = 7782176087540717680L;
+
 }
