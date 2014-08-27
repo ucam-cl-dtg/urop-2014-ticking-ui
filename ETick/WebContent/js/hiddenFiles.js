614aac28acc57c18fd669dfe589b448e1712479e
Kovacsics Robert (Arch) <kovirobi@gmail.com>
2014-08-27 09:08:20 +0100

    Added file-hiding.

---
+++ ETick/WebContent/js/hiddenFiles.js
@@ -0,0 +1,31 @@
+function isHidden (file)
+{
+    if (!String.prototype.endsWith) {
+        Object.defineProperty(String.prototype, 'endsWith', {
+            value: function (searchString, position) {
+                var subjectString = this.toString();
+                if (position === undefined || position > subjectString.length) {
+                    position = subjectString.length;
+                }
+                position -= searchString.length;
+                var lastIndex = subjectString.indexOf(searchString, position);
+                return lastIndex !== -1 && lastIndex === position;
+            }
+        });
+    }
+
+    if (!String.prototype.startsWith) {
+        Object.defineProperty(String.prototype, 'startsWith', {
+            enumerable: false,
+            configurable: false,
+            writable: false,
+            value: function (searchString, position) {
+                position = position || 0;
+                return this.lastIndexOf(searchString, position) === position;
+            }
+        });
+    }
+
+    return file.name.startsWith(".")
+        || file.name.endsWith(".class");
+}
